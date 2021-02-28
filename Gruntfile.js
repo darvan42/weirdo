d24e2ea8b633988d7d47a592f55a0732db16eaf1
Darvan42 <darvan42@mailbox.org>
2021-02-28 16:53:47 +0100

    Configure grunt to upload to screeps account

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,5 +3,27 @@
  * @param grunt {IGrunt}
  */
 module.exports = function (grunt) {
-  //TODO add screeps config
+
+  //Import credentials from config file
+  let config = require('./screeps_credentials.json')
+
+  //Get config from parameters or use options from file
+  let branch = grunt.option('branch') || config.branch
+  let email = grunt.option('email') || config.email
+  let token = grunt.option('token') || config.token
+
+  grunt.loadNpmTasks('grunt-screeps')
+
+  grunt.initConfig({
+    screeps: {
+      options: {
+        email: email,
+        token: token,
+        branch: branch
+      },
+      dist: {
+        src: ['src/*.js']
+      }
+    }
+  })
 }
