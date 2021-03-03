062e2039819e19b41aa839a24a94e66e67e76911
Darvan42 <darvan42@mailbox.org>
2021-03-03 17:01:25 +0100

    Add task fill to starter role

--- src/role_starter.js
+++ src/role_starter.js
@@ -2,9 +2,13 @@ module.exports = starterGetNextAction
 
 const getActionFrom = require('./task-combine')
 const actionMine = require('./task_mine')
+const taskFill = require('./task_fill')
 
 function starterGetNextAction (creep) {
   if (creep.store.getFreeCapacity() > 0) {
     return getActionFrom([actionMine], creep)
+  } else {
+    const curryFill = creep1 => taskFill(STRUCTURE_SPAWN, creep1)
+    return getActionFrom([curryFill], creep)
   }
 }
