8e15fd13c51450d300077052df2db0ee3a1c16e8
Darvan42 <darvan42@mailbox.org>
2021-03-05 13:55:06 +0100

    Add upgrade task

--- src/task_upgrade.js
+++ src/task_upgrade.js
@@ -7,7 +7,7 @@
 module.exports = function (creep) {
   const controller = creep.room.controller
   if (controller) {
-    if (creep.pos.inRangeTo(controller, 3)) {
+    if (creep.pos.inRangeTo(controller.pos, 3)) {
       return () => creep.upgradeController(controller)
     } else {
       return () => creep.moveTo(controller)
