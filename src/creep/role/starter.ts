2b3014d0c1cad65a5f00ce5fed83bca77a62ebb7
darvan42 <darvan42@mailbox.org>
2021-08-31 12:33:59 +0200

    Remove let's go message

--- src/creep/role/starter.ts
+++ src/creep/role/starter.ts
@@ -1,7 +1,6 @@
 export const ROLE = 'starter'
 
 export default function (creep: Creep) {
-  creep.say('Let\'s go!')
   if (creep.store.getFreeCapacity(RESOURCE_ENERGY) > 0) {
     const target = creep.pos.findClosestByPath(FIND_SOURCES) as Source
     if (creep.harvest(target) === ERR_NOT_IN_RANGE) {
