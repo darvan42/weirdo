69cea91d98a738f83c0a7203dfaa280269999dd1
darvan42 <darvan42@mailbox.org>
2021-08-31 12:33:17 +0200

    Add creepTick for every creep

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import spawnTick from './spawn/tick'
+import creepTick from './creep/tick'
+
 export function loop () {
   for (const spawnName in Game.spawns) {
     spawnTick(Game.spawns[spawnName])
+  }
+  for (const creepName in Game.creeps) {
+    creepTick(Game.creeps[creepName])
   }
 }
