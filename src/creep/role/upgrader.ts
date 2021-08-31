b56660055436364f918ab4c13215d849d987ee98
darvan42 <darvan42@mailbox.org>
2021-08-31 13:02:48 +0200

    Implement upgrader role logic

--- src/creep/role/upgrader.ts
+++ src/creep/role/upgrader.ts
@@ -1,5 +1,23 @@
 export const ROLE = 'upgrader'
 
 export default function (creep: Creep) {
-  creep.say('Upgrade!')
+  if (creep.memory.upgrading === true) {
+    if (creep.store.getUsedCapacity(RESOURCE_ENERGY) > 0) {
+      const controller = creep.room.controller as StructureController
+      if (creep.upgradeController(controller) === ERR_NOT_IN_RANGE) {
+        creep.moveTo(controller)
+      }
+    } else {
+      creep.memory.upgrading = false
+    }
+  } else {
+    if (creep.store.getFreeCapacity(RESOURCE_ENERGY) > 0) {
+      const target = creep.pos.findClosestByPath(FIND_SOURCES) as Source
+      if (creep.harvest(target) === ERR_NOT_IN_RANGE) {
+        creep.moveTo(target)
+      }
+    } else {
+      creep.memory.upgrading = true
+    }
+  }
 }
