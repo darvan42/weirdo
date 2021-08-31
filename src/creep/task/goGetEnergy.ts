c195c92f546e65dee83a64815403010a23077521
darvan42 <darvan42@mailbox.org>
2021-08-31 13:10:55 +0200

    Add dropped energy pickup logic

--- src/creep/task/goGetEnergy.ts
+++ src/creep/task/goGetEnergy.ts
@@ -1,6 +1,15 @@
 export default function (creep: Creep) {
-  const target = creep.pos.findClosestByPath(FIND_SOURCES) as Source
-  if (creep.harvest(target) === ERR_NOT_IN_RANGE) {
-    creep.moveTo(target)
+  // Checks if there is dropped energy
+  const droppedEnergy = creep.room.find(FIND_DROPPED_RESOURCES, { filter: RESOURCE_ENERGY })
+  if (droppedEnergy.length > 0) {
+    const target = creep.pos.findClosestByPath(droppedEnergy) as Resource
+    if (creep.pickup(target) === ERR_NOT_IN_RANGE) {
+      creep.moveTo(target)
+    }
+  } else {
+    const target = creep.pos.findClosestByPath(FIND_SOURCES) as Source
+    if (creep.harvest(target) === ERR_NOT_IN_RANGE) {
+      creep.moveTo(target)
+    }
   }
 }
