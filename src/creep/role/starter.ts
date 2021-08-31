fa66bc58a04659ae245b21479bfb9d56de90ba66
darvan42 <darvan42@mailbox.org>
2021-08-31 12:33:29 +0200

    Implement basic starter logic

--- src/creep/role/starter.ts
+++ src/creep/role/starter.ts
@@ -2,4 +2,15 @@ export const ROLE = 'starter'
 
 export default function (creep: Creep) {
   creep.say('Let\'s go!')
+  if (creep.store.getFreeCapacity(RESOURCE_ENERGY) > 0) {
+    const target = creep.pos.findClosestByPath(FIND_SOURCES) as Source
+    if (creep.harvest(target) === ERR_NOT_IN_RANGE) {
+      creep.moveTo(target)
+    }
+  } else {
+    const target = creep.pos.findClosestByPath(FIND_MY_SPAWNS) as StructureSpawn
+    if (creep.transfer(target, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+      creep.moveTo(target)
+    }
+  }
 }
