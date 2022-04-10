ce1f1b08e691d4999fa4f899cec52fb453888329
darvan42 <darvan42@mailbox.org>
2022-04-10 16:32:03 +0200

    Rewrite goDistributeEnergy task to include creeps as targets

--- src/creep/task/goDistributeEnergy.ts
+++ src/creep/task/goDistributeEnergy.ts
@@ -1,13 +1,21 @@
 import goTransferEnergy from './goTransferEnergy'
+import { getRoleMembersInRoom } from '../role/utils'
 
 export default function (creep: Creep) {
   const spawns = creep.room.find(FIND_MY_SPAWNS)
-  const lowestEnergySpawn = spawns.reduce((prev, current) => {
-    return (prev.store.getFreeCapacity(RESOURCE_ENERGY) > current.store.getFreeCapacity(RESOURCE_ENERGY)) ? prev : current
-  })
-  if (lowestEnergySpawn.store.getFreeCapacity(RESOURCE_ENERGY) === 0) {
-    creep.say('All spawns full!')
-  } else {
+  const lowestEnergySpawn = getLowestEnergyThing(spawns)
+  if (lowestEnergySpawn.store.getFreeCapacity(RESOURCE_ENERGY) > 0) {
     goTransferEnergy(creep, lowestEnergySpawn)
+  } else {
+    // TODO Rollennamen Konstanten besser managen
+    const upgraders = getRoleMembersInRoom(creep.room, 'upgrader')
+    const lowestUpgrader = getLowestEnergyThing(upgraders)
+    goTransferEnergy(creep, lowestUpgrader)
   }
+}
+
+function getLowestEnergyThing (things: Array<AnyCreep | AnyStoreStructure>) {
+  return things.reduce((prev, curr) => {
+    return (prev.store.getFreeCapacity(RESOURCE_ENERGY) > curr.store.getFreeCapacity(RESOURCE_ENERGY)) ? prev : curr
+  })
 }
