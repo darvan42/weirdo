7472c8e8892f56a1f027fa9465616b780211f276
darvan42 <darvan42@mailbox.org>
2022-04-11 09:10:55 +0200

    Refactor goGetEnergy to take an optional position argument

--- src/creep/task/goGetEnergy.ts
+++ src/creep/task/goGetEnergy.ts
@@ -1,10 +1,11 @@
 import goHarvestSource from './goHarvestSource'
 
-export default function (creep: Creep) {
+export default function (creep: Creep, near?: RoomPosition) {
   // Checks if there is dropped energy
   const droppedEnergy = creep.room.find(FIND_DROPPED_RESOURCES, { filter: RESOURCE_ENERGY })
   if (droppedEnergy.length > 0) {
-    const target = creep.pos.findClosestByPath(droppedEnergy) as Resource
+    const position = near != null ? near : creep.pos
+    const target = position.findClosestByPath(droppedEnergy) as Resource
     if (creep.pickup(target) === ERR_NOT_IN_RANGE) {
       creep.moveTo(target)
     }
