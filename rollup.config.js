9e024a007807f9a04738e913a0f6f15d17e8df27
darvan42 <darvan42@mailbox.org>
2022-04-16 14:15:44 +0200

    Automate release process to include wasm

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,7 @@
+import replace from '@rollup/plugin-replace'
 import typescript from '@rollup/plugin-typescript'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
 import screeps from 'rollup-plugin-screeps'
 
 const dest = process.env.DEST
@@ -6,13 +9,27 @@ let dryRun = true
 if (dest) dryRun = false
 export default {
   input: 'src/main.ts',
+  external: ['weirdo_architect_bg.wasm'],
   output: {
     file: 'dist/main.js',
     sourcemap: true,
     format: 'cjs'
   },
   plugins: [
+    replace({
+      preventAssignment: true,
+      delimiters: ['', ''],
+      values: {
+        "const path = require('path').join(__dirname, 'weirdo_architect_bg.wasm');": '',
+        "require('fs').readFileSync(path);": "require('weirdo_architect_bg.wasm');"
+      }
+    }),
     typescript(),
-    screeps({ configFile: './screeps.json', dryRun: dryRun })
+    resolve(),
+    commonjs(),
+    screeps({
+      configFile: './screeps.json',
+      dryRun: dryRun
+    })
   ]
 }
