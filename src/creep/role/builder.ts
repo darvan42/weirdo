34145a741237e9516bfdbd8b28df43b239b990d2
Darvan42 <darvan42@mailbox.org>
2023-01-06 15:21:38 +0100

    feat: Connect builder role with main AI

--- src/creep/role/builder.ts
+++ src/creep/role/builder.ts
@@ -1,12 +1,29 @@
 import goGetEnergy from '../task/goGetEnergy'
 import goBuildStructure from '../task/goBuildStructure'
+import { generateName, getRoleMembersInRoom } from './utils'
 
 export const ROLENAME = 'builder'
 
 export function eachTick (creep: Creep) {
-  if (creep.memory.building && creep.store.getUsedCapacity(RESOURCE_ENERGY) > 0) {
+  if (creep.store.getUsedCapacity(RESOURCE_ENERGY) > 0) {
     goBuildStructure(creep)
   } else {
     goGetEnergy(creep)
   }
+}
+
+export function getNumberBuilderNeeded (room: Room): number {
+  const sites = room.find(FIND_MY_CONSTRUCTION_SITES).length
+  const builders = getRoleMembersInRoom(room, ROLENAME).length
+
+  if (sites == 0) return 0
+  const sitesBuilderDiff = sites - builders
+  if (sitesBuilderDiff <= 0) return 0
+  if (sitesBuilderDiff >= 3) return 3
+  return sitesBuilderDiff
+}
+
+export function spawnBuilder (spawn: StructureSpawn) {
+  // TODO optimize body
+  spawn.spawnCreep([WORK, CARRY, MOVE, MOVE], generateName(ROLENAME))
 }
--- src/spawn/tick.ts
+++ src/spawn/tick.ts
@@ -2,6 +2,7 @@ import { areStarterNeeded, spawnStarter } from '../creep/role/starter'
 import { getNumberMinerNeeded, spawnMiner } from '../creep/role/miner'
 import { getNumberUpgraderNeeded, spawnUpgrader } from '../creep/role/upgrader'
 import { getNumberCarryNeeded, spawnCarry } from '../creep/role/carry'
+import { getNumberBuilderNeeded, spawnBuilder } from '../creep/role/builder'
 
 export default function tick (spawn: StructureSpawn) {
   if (spawn.spawning != null) return
@@ -23,6 +24,10 @@ export default function tick (spawn: StructureSpawn) {
     {
       spawnFunction: spawnMiner,
       numberNeeded: getNumberMinerNeeded(room)
+    },
+    {
+      spawnFunction: spawnBuilder,
+      numberNeeded: getNumberBuilderNeeded(room)
     }
   ]
   const mostNeeded = functionNeededPairs.reduce((prev, curr) => {
